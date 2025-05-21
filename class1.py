75d0efe9e64b3f9512271865351525bbcf2ef2c6
Terminator19220 <sinaijudah@gmail.com>
2025-05-21 12:21:41 -0400

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,33 @@
+#print("hello world")
+#Activity 1-print statement
+print("welcome sinai")
+"""In this activity, students will learn the basics of Python programming
+by using print statements to display text and perform simple calculations.
+They will also learn how to use comments to document their code."""
+
+#Activity 2-Algorithm
+print("This is how to make BBQ")
+
+print("step1:Get a Grill, Smoker, or a Combo") 
+
+print("step2:Assemble Your Grill")
+
+print("step3:Clean Up Your Grill")
+
+print("step4:Prepare Fuel")
+
+print("step5:Ready Your Food")
+
+print("step6: Power Up Your Grill")
+
+print("step7:Prep for Flavoring")
+
+print("step8:All Set for Cooking")
+
+print("step9:Use the Cooking Tools")
+
+print("step10:Check Temperature")
+
+print("step11:BBQ Food Is Ready")
+
+print("step12:Clean While Still Warm")
